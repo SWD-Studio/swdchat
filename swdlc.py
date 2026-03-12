f652a13c11bc9608f7ed250ec91831fe3d9d240e
gqm321 <62005618+gqm321@users.noreply.github.com>
2026-03-12 20:07:20 +0800

    修改swdlc.py的年份信息

--- swdlc.py
+++ swdlc.py
@@ -1,4 +1,4 @@
-#    Copyright (C) 2020-2025  SWD Studio
+#    Copyright (C) 2020-2026  SWD Studio
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
